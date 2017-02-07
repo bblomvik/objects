d2dfeae06cb8405dba9b3cdf79bae5a79d3ede83
brynjar.blomvik <brynjar.blomvik@gmail.com>
2017-02-07 15:28:05 +0100

    refactor start pt1

--- zuul-egen-refactor/Game.java
+++ zuul-egen-refactor/Game.java
@@ -18,8 +18,7 @@
 public class Game 
 {
     private Parser parser;
-    private Room currentRoom;
-    private Room previousRoom;
+    private Player player1;
     private Item itemInRoom;
 
     /**
@@ -27,66 +26,12 @@ public class Game
      */
     public Game() 
     {
-        createRooms();
         parser = new Parser();
+        player1 = new Player();
+        player1.createRooms();
         play();
     }
 
-    /**
-     * Create all the rooms and link their exits together.
-     */
-    private void createRooms()
-    {
-        Room outside, theater, pub, lab, office, cellar;
-        Item flower, sword, potion, key;
-
-        // create the rooms
-        outside = new Room("outside the main entrance of the university");
-        theater = new Room("in a lecture theater");
-        pub = new Room("in the campus pub");
-        lab = new Room("in a computing lab");
-        office = new Room("in the computing admin office");
-        cellar = new Room("below a secret hatch in the office");
-
-        // initialise room exits
-        outside.setExit("east", theater);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
-        
-        theater.setExit("west", outside);
-        
-        pub.setExit("east", outside);
-        
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
-        
-        office.setExit("west", lab);
-        office.setExit("down", cellar);
-        
-        cellar.setExit("up", office);
-                
-        /////////////////////////////////////////////
-        
-        //create items
-        flower = new Item("Flower: a beautiful red flower.", 3);
-        sword = new Item("Longsword: a powerful weapon", 2);
-        potion = new Item("Potion, a healing drink", 1);
-        key = new Item("Key: this will open a locked door somewhere. but it's heavy!", 5);
-        sword.printItem();
-        
-        // place items in room
-        outside.setItem("flower", flower);
-        pub.setItem("potion", potion);
-        office.setItem("key", key);
-        cellar.setItem("sword", sword);
-        
-        
-        /////////////////////////////////////////////
-        
-        
-        currentRoom = outside;  // start game outside
-        itemInRoom = flower;    // places a flower outside
-    }
 
     /**
      *  Main play routine.  Loops until end of play.
@@ -117,37 +62,10 @@ public class Game
         System.out.println("Type 'help' if you need help.");
         System.out.println();
         System.out.println();
-        printLocationInfo();
+        player1.printLocationInfo();
     }
-    
-    /**
-     * Prints your current location and possible exits
-     */
-    public void printLocationInfo()
-    {       
-        System.out.println(currentRoom.getLongDescription());
-    }
-    
-    /**
-     * Take a look around the room and print description and exits
-     */
-    public void look() {
-        System.out.println(currentRoom.getLongDescription());
-    }
-    
-    /**
-     * Engage battle with creature
-     */
-    public void fight() {
-        boolean creaturePresent = false;
-        if (creaturePresent = false) {
-            System.out.println("You look around and see no foe");
-        }
-        else {
-            System.out.println("You attack the monster with your bare hands!" + " \n" + "Only one of you will remain victorious"); 
-        }
-    }
-    
+
+
     /**
      * Given a command, process (that is: execute) the command.
      * @param command The command to be processed.
@@ -167,19 +85,19 @@ public class Game
             System.out.println(parser.showCommands());
         }
         else if (commandWord.equals("go")) {
-            goRoom(command);
+            player1.goRoom(command);
         }
         else if (commandWord.equals("back")) {
-            back();
+            player1.back();
         }
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
         }
         else if (commandWord.equals("look")) {
-            look();
+            player1.look();
         }
         else if (commandWord.equals("fight")) {
-            fight();
+            player1.fight();
         }
 
         return wantToQuit;
@@ -199,54 +117,7 @@ public class Game
         System.out.println();
         System.out.println("Your command words are:");
         parser.showCommands();
-    }
-
-    /** 
-     * Try to go in one direction. If there is an exit, enter
-     * the new room, otherwise print an error message.
-     */
-    private void goRoom(Command command) 
-    {
-        if(!command.hasSecondWord()) {
-            // if there is no second word, we don't know where to go...
-            System.out.println("Go where?");
-            return;
-        }
-
-        String direction = command.getSecondWord();
-
-        // Try to leave current room.
-        Room nextRoom = null;
-        nextRoom = currentRoom.getExit(direction);
-
-        if (nextRoom == null) {
-            System.out.println("There is no door!");
-        }
-        else {
-            previousRoom = currentRoom;
-            currentRoom = nextRoom;          
-            System.out.println(currentRoom.getLongDescription());
-            System.out.println();
-            System.out.println();
-        }
-    }
-    
-    /**
-     * Go to previous room
-     */
-    private void back() {
-        if (previousRoom == null) {
-            System.out.println("Sorry, you can't go back");
-        }
-        else {
-            Room nextRoom = previousRoom;
-            previousRoom = currentRoom;
-            currentRoom = nextRoom;
-            System.out.println(currentRoom.getLongDescription());
-            System.out.println();
-            System.out.println();
-        }
-    }
+    }   
 
     /** 
      * "Quit" was entered. Check the rest of the command to see
--- zuul-egen-refactor/Player.java
+++ zuul-egen-refactor/Player.java
@@ -7,27 +7,146 @@
  */
 public class Player
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private String name;
+    private int totalWeight;
+    private Room currentRoom;
+    private Room previousRoom;
 
     /**
      * Constructor for objects of class Player
+     * Creates a player with generic name and total weight 2000g
      */
     public Player()
     {
-        // initialise instance variables
-        x = 0;
+        name = "playerName";
+        totalWeight = 2000;
     }
 
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     * Create all the rooms and link their exits together.
      */
-    public int sampleMethod(int y)
+    public void createRooms()
     {
-        // put your code here
-        return x + y;
+        Room outside, theater, pub, lab, office, cellar;
+        Item flower, sword, potion, key;
+
+        // create the rooms
+        outside = new Room("outside the main entrance of the university");
+        theater = new Room("in a lecture theater");
+        pub = new Room("in the campus pub");
+        lab = new Room("in a computing lab");
+        office = new Room("in the computing admin office");
+        cellar = new Room("below a secret hatch in the office");
+
+        // initialise room exits
+        outside.setExit("east", theater);
+        outside.setExit("south", lab);
+        outside.setExit("west", pub);
+
+        theater.setExit("west", outside);
+
+        pub.setExit("east", outside);
+
+        lab.setExit("north", outside);
+        lab.setExit("east", office);
+
+        office.setExit("west", lab);
+        office.setExit("down", cellar);
+
+        cellar.setExit("up", office);
+
+        /////////////////////////////////////////////
+
+        //create items
+        flower = new Item("Flower: a beautiful red flower.", 3);
+        sword = new Item("Longsword: a powerful weapon", 2);
+        potion = new Item("Potion, a healing drink", 1);
+        key = new Item("Key: this will open a locked door somewhere. but it's heavy!", 5);
+        sword.printItem();
+
+        // place items in room
+        outside.setItem("flower", flower);
+        pub.setItem("potion", potion);
+        office.setItem("key", key);
+        cellar.setItem("sword", sword);
+
+        /////////////////////////////////////////////
+        
+        currentRoom = outside;  // start game outside
+    }
+
+    /** 
+     * Try to go in one direction. If there is an exit, enter
+     * the new room, otherwise print an error message.
+     */
+    public void goRoom(Command command) 
+    {
+        if(!command.hasSecondWord()) {
+            // if there is no second word, we don't know where to go...
+            System.out.println("Go where?");
+            return;
+        }
+
+        String direction = command.getSecondWord();
+
+        // Try to leave current room.
+        Room nextRoom = null;
+        nextRoom = currentRoom.getExit(direction);
+
+        if (nextRoom == null) {
+            System.out.println("There is no door!");
+        }
+        else {
+            previousRoom = currentRoom;
+            currentRoom = nextRoom;          
+            System.out.println(currentRoom.getLongDescription());
+            System.out.println();
+            System.out.println();
+        }
+    }
+
+    /**
+     * Go to previous room
+     */
+    public void back() {
+        if (previousRoom == null) {
+            System.out.println("Sorry, you can't go back");
+        }
+        else {
+            Room nextRoom = previousRoom;
+            previousRoom = currentRoom;
+            currentRoom = nextRoom;
+            System.out.println(currentRoom.getLongDescription());
+            System.out.println();
+            System.out.println();
+        }
+    }
+
+    /**
+     * Prints your current location and possible exits
+     */
+    public void printLocationInfo()
+    {       
+        System.out.println(currentRoom.getLongDescription());
+    }
+
+    /**
+     * Take a look around the room and print description and exits
+     */
+    public void look() {
+        System.out.println(currentRoom.getLongDescription());
+    }
+
+    /**
+     * Engage battle with creature
+     */
+    public void fight() {
+        boolean creaturePresent = false;
+        if (creaturePresent = false) {
+            System.out.println("You look around and see no foe");
+        }
+        else {
+            System.out.println("You attack the monster with your bare hands!" + " \n" + "Only one of you will remain victorious"); 
+        }
     }
 }
